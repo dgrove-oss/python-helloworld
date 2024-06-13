37ce87cd543664b282a767a2d9280afd5c3aa7dd
David Grove <groved@us.ibm.com>
2024-06-13 18:06:50 -0400

    add ability to sleep and exit with non-zero code

--- helloworld/main.py
+++ helloworld/main.py
@@ -2,6 +2,7 @@
 
 import argparse
 import sys
+import time
 
 import helloworld
 
@@ -10,17 +11,19 @@ parser = argparse.ArgumentParser(
         description='A simple example program to print a friendly greeting.')
 parser.add_argument('--version', action='version',
         version='helloworld ' + helloworld.__version__)
+parser.add_argument('--sleep', type=float, default=1, help="Seconds to sleep")
+parser.add_argument('--exit_code', type=int, default=0, help="code to exit with")
 
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    # The helloworld program doesn't expect any arguments.
-    # This just checks for the special --version and --help arguments and
-    # ensures the user hasn't passed any other unrecognized arguments.
-    parser.parse_args(argv[1:])
+    args = parser.parse_args(argv[1:])
 
     print("Hello, world")
+    print(f'Sleeping for {args.sleep} seconds')
+    time.sleep(args.sleep)
+    print("Goodbye")
 
-    return 0
+    return args.exit_code
